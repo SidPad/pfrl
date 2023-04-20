f5a4837fc5db0c6c2e90fe9fa2af0ce23b64827e
SidPad <56720486+SidPad@users.noreply.github.com>
2023-04-20 12:14:07 +0900

    Update without 100 Hz sampling

--- pfrl/experiments/evaluator.py
+++ pfrl/experiments/evaluator.py
@@ -29,35 +29,32 @@ def _run_episodes(
     reset = True
     timer = 0
     while not terminate:
-        if timer % 5 == 0:
-            if reset:
-                obs = env.reset()
-                done = False
-                test_r = 0
-                episode_len = 0
-                info = {}
-            a = agent.act(obs)
-            obs, r, done, info = env.step(a)
-            test_r += r
-            episode_len += 1
-            timestep += 1
-            reset = done or episode_len == max_episode_len or info.get("needs_reset", False)
-            agent.observe(obs, r, done, reset)
-            if reset:
-                logger.info(
-                    "evaluation episode %s length:%s R:%s", len(scores), episode_len, test_r
-                )
-                # As mixing float and numpy float causes errors in statistics
-                # functions, here every score is cast to float.
-                scores.append(float(test_r))
-                lengths.append(float(episode_len))
-            if n_steps is None:
-                terminate = len(scores) >= n_episodes
-            else:
-                terminate = timestep >= n_steps
+        if reset:
+            obs = env.reset()
+            done = False
+            test_r = 0
+            episode_len = 0
+            info = {}
+        a = agent.act(obs)
+        obs, r, done, info = env.step(a)
+        test_r += r
+        episode_len += 1
+        timestep += 1
+        reset = done or episode_len == max_episode_len or info.get("needs_reset", False)
+        agent.observe(obs, r, done, reset)
+        if reset:
+            logger.info(
+                "evaluation episode %s length:%s R:%s", len(scores), episode_len, test_r
+            )
+            # As mixing float and numpy float causes errors in statistics
+            # functions, here every score is cast to float.
+            scores.append(float(test_r))
+            lengths.append(float(episode_len))
+        if n_steps is None:
+            terminate = len(scores) >= n_episodes
         else:
-            env.step(empty_action)
-        timer += 1
+            terminate = timestep >= n_steps
+        
     # If all steps were used for a single unfinished episode
     if len(scores) == 0:
         scores.append(float(test_r))
@@ -133,89 +130,85 @@ def _batch_run_episodes(
     timestep = 0
     timer = 0
     while True:
-        if timer % 5 == 0:
-            # a_t
-            actions = agent.batch_act(obss, actions)
-            timestep += 1
-            # o_{t+1}, r_{t+1}
-            obss, rs, dones, infos = env.step(actions)
-            episode_r += rs
-            episode_len += 1
-            # Compute mask for done and reset
-            if max_episode_len is None:
-                resets = np.zeros(num_envs, dtype=bool)
-            else:
-                resets = episode_len == max_episode_len
-            resets = np.logical_or(
-                resets, [info.get("needs_reset", False) for info in infos]
-            )
-
-            # Make mask. 0 if done/reset, 1 if pass
-            end = np.logical_or(resets, dones)
-            not_end = np.logical_not(end)
-
-            for index in range(len(end)):
-                if end[index]:
-                    episode_returns[episode_indices[index]] = episode_r[index]
-                    episode_lengths[episode_indices[index]] = episode_len[index]
-                    # Give the new episode an a new episode index
-                    episode_indices[index] = episode_idx
-                    episode_idx += 1
-
-            episode_r[end] = 0
-            episode_len[end] = 0
-
-            # find first unfinished episode
-            first_unfinished_episode = 0
-            while first_unfinished_episode in episode_returns:
-                first_unfinished_episode += 1
-
-            # Check for termination conditions
-            eval_episode_returns = []
-            eval_episode_lens = []
-            if n_steps is not None:
-                total_time = 0
-                for index in range(first_unfinished_episode):
-                    total_time += episode_lengths[index]
-                    # If you will run over allocated steps, quit
-                    if total_time > n_steps:
-                        break
-                    else:
-                        eval_episode_returns.append(episode_returns[index])
-                        eval_episode_lens.append(episode_lengths[index])
-                termination_conditions = total_time >= n_steps
-                if not termination_conditions:
-                    unfinished_index = np.where(
-                        episode_indices == first_unfinished_episode
-                    )[0]
-                    if total_time + episode_len[unfinished_index] >= n_steps:
-                        termination_conditions = True
-                        if first_unfinished_episode == 0:
-                            eval_episode_returns.append(episode_r[unfinished_index])
-                            eval_episode_lens.append(episode_len[unfinished_index])
-
-            else:
-                termination_conditions = first_unfinished_episode >= n_episodes
-                if termination_conditions:
-                    # Get the first n completed episodes
-                    for index in range(n_episodes):
-                        eval_episode_returns.append(episode_returns[index])
-                        eval_episode_lens.append(episode_lengths[index])
-
-            if termination_conditions:
-                # If this is the last step, make sure the agent observes reset=True
-                resets.fill(True)
-
-            # Agent observes the consequences.
-            agent.batch_observe(obss, rs, dones, resets)
-
-            if termination_conditions:
-                break
-            else:
-                obss = env.reset(not_end)
+        # a_t
+        actions = agent.batch_act(obss, actions)
+        timestep += 1
+        # o_{t+1}, r_{t+1}
+        obss, rs, dones, infos = env.step(actions)
+        episode_r += rs
+        episode_len += 1
+        # Compute mask for done and reset
+        if max_episode_len is None:
+            resets = np.zeros(num_envs, dtype=bool)
         else:
-            env.step(empty_action)
-        timer += 1
+            resets = episode_len == max_episode_len
+        resets = np.logical_or(
+            resets, [info.get("needs_reset", False) for info in infos]
+        )
+
+        # Make mask. 0 if done/reset, 1 if pass
+        end = np.logical_or(resets, dones)
+        not_end = np.logical_not(end)
+
+        for index in range(len(end)):
+            if end[index]:
+                episode_returns[episode_indices[index]] = episode_r[index]
+                episode_lengths[episode_indices[index]] = episode_len[index]
+                # Give the new episode an a new episode index
+                episode_indices[index] = episode_idx
+                episode_idx += 1
+
+        episode_r[end] = 0
+        episode_len[end] = 0
+
+        # find first unfinished episode
+        first_unfinished_episode = 0
+        while first_unfinished_episode in episode_returns:
+            first_unfinished_episode += 1
+
+        # Check for termination conditions
+        eval_episode_returns = []
+        eval_episode_lens = []
+        if n_steps is not None:
+            total_time = 0
+            for index in range(first_unfinished_episode):
+                total_time += episode_lengths[index]
+                # If you will run over allocated steps, quit
+                if total_time > n_steps:
+                    break
+                else:
+                    eval_episode_returns.append(episode_returns[index])
+                    eval_episode_lens.append(episode_lengths[index])
+            termination_conditions = total_time >= n_steps
+            if not termination_conditions:
+                unfinished_index = np.where(
+                    episode_indices == first_unfinished_episode
+                )[0]
+                if total_time + episode_len[unfinished_index] >= n_steps:
+                    termination_conditions = True
+                    if first_unfinished_episode == 0:
+                        eval_episode_returns.append(episode_r[unfinished_index])
+                        eval_episode_lens.append(episode_len[unfinished_index])
+
+        else:
+            termination_conditions = first_unfinished_episode >= n_episodes
+            if termination_conditions:
+                # Get the first n completed episodes
+                for index in range(n_episodes):
+                    eval_episode_returns.append(episode_returns[index])
+                    eval_episode_lens.append(episode_lengths[index])
+
+        if termination_conditions:
+            # If this is the last step, make sure the agent observes reset=True
+            resets.fill(True)
+
+        # Agent observes the consequences.
+        agent.batch_observe(obss, rs, dones, resets)
+
+        if termination_conditions:
+            break
+        else:
+            obss = env.reset(not_end)
 
     for i, (epi_len, epi_ret) in enumerate(
         zip(eval_episode_lens, eval_episode_returns)
