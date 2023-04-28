3554f72fef3c37149d30048b46cabfc93164e803
SidPad <56720486+SidPad@users.noreply.github.com>
2023-04-28 11:20:08 +0900

    Update SINGLEGRULoco_HID-Debug

--- pfrl/agents/soft_actor_critic.py
+++ pfrl/agents/soft_actor_critic.py
@@ -828,7 +828,9 @@ class MTSoftActorCritic(AttributeSavingMixin, BatchAgent):
             indicesAA = []
             # indicesBB = []
             # indicesCC = []
-            
+            print(print(ep_len_actual)
+            print(ep_len_actual_sum1)
+            print(ep_len_actual_sum2)
             for j in range(len(ep_len_actual_sum2)): 
                 if j in indicesA:
                     random_indexA = random.randint(ep_len_actual_sum2[j], ep_len_actual_sum1[j] - self.seq_len)
