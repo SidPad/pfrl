14c051697359ba5f06d8de3d0a1cdaeb4f6c2bcf
SidPad <56720486+SidPad@users.noreply.github.com>
2023-06-26 16:38:37 +0900

    Update Debug

--- pfrl/agents/soft_actor_critic.py
+++ pfrl/agents/soft_actor_critic.py
@@ -1097,6 +1097,7 @@ class MTSoftActorCritic(AttributeSavingMixin, BatchAgent):
         # compute grad norms
         norms = []        
         for w_i, L_i in zip(self.weights, losses):
+            losses.requires_grad_(False)
             dlidW = torch.autograd.grad(L_i, last_shared_params, retain_graph=True)[0]
             norms.append(torch.norm(w_i * dlidW))
 
