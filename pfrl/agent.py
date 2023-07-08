17dcf21b2c5ec8681dcb30ebe80b99a2528a689b
SidPad <56720486+SidPad@users.noreply.github.com>
2023-07-08 16:23:44 +0900

    Update DebugAttribute

--- pfrl/agent.py
+++ pfrl/agent.py
@@ -85,6 +85,7 @@ class AttributeSavingMixin(object):
     def __save(self, dirname: str, ancestors: List[Any]):
         os.makedirs(dirname, exist_ok=True)
         ancestors.append(self)
+        print(self.saved_attributes)
         for attr in self.saved_attributes:
             assert hasattr(self, attr)
             attr_value = getattr(self, attr)
